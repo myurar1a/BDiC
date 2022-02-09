51621dcc6d848a687330a06ecd6e1af5ff9bde02
myurar1a <sirometroid1235@outlook.jp>
2022-02-09 16:31:43 +0900

    Fix: [src][Functions] TimeRelated の命名規則の統一

--- src/Functions/TimeRelated.cs
+++ src/Functions/TimeRelated.cs
@@ -10,25 +10,27 @@ namespace BDiC
     public class TimeRelated
     {
         // 現在時刻
-        private DateTime current;
+        private DateTime _current;
         // 今日の曜日
-        private DayOfWeek todayDayOfWeek;
-        
+        private DayOfWeek _todayDayOfWeek;
+
+        // 今日の日付
+        private DateTime _date;
         // BDから取得した日付
-        private String latestDate;
+        private String? _latestDate;
         // 今日と更新日の差
-        private int dateCompare;
+        private int _dateCompare;
 
         // 処理開始時間 (引数値)
-        private DateTime before;
+        private DateTime _before;
         // 現在と処理開始時間の差分
-        private TimeSpan delta;
+        private TimeSpan _delta;
 
         public void ExctionDateDecision()
         {
-            current = DateTime.Now;
-            todayDayOfWeek = current.DayOfWeek;
-            if (todayDayOfWeek != DayOfWeek.Saturday || todayDayOfWeek != DayOfWeek.Sunday)
+            this._current = DateTime.Now;
+            this._todayDayOfWeek = this._current.DayOfWeek;
+            if (this._todayDayOfWeek != DayOfWeek.Saturday || this._todayDayOfWeek != DayOfWeek.Sunday)
             {
                 Console.WriteLine("今日は週末なので、お休みです！");
                 Environment.Exit(0);
@@ -39,12 +41,12 @@ namespace BDiC
 
         public bool ConfirmationOfArrivalDate(String latestDateRaw)
         {
-            this.latestDate = latestDateRaw.Trim();
-            current = DateTime.Now.Date;
-            dateCompare = DateTime.Compare(current, DateTime.Parse(latestDate));
+            this._latestDate = latestDateRaw.Trim();
+            this._date = DateTime.Now.Date;
+            this._dateCompare = DateTime.Compare(this._date, DateTime.Parse(this._latestDate));
             // 今日と同日なら0、前の日なら1
 
-            if (dateCompare == 0)
+            if (this._dateCompare == 0)
                 return true;
             else
                 return false;
@@ -52,11 +54,11 @@ namespace BDiC
 
         public TimeSpan WaitTime(DateTime before)
         {
-            this.before = before;
-            current = DateTime.Now;
-            delta = current - before;
+            this._before = before;
+            this._current = DateTime.Now;
+            this._delta = this._current - before;
 
-            return new TimeSpan(0, 0, 0, 5) - delta;
+            return new TimeSpan(0, 0, 0, 5) - this._delta;
         }
     }
 }
